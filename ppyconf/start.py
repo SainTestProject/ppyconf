d20ac26f03064e3609709c7fbaf1a882bbe7da80
sain <467676134@qq.com>
2014-08-20 09:31:26 +0800

    文件读取方式改为：with open() as var 添加注释

--- ppyconf/start.py
+++ ppyconf/start.py
@@ -1,20 +1,31 @@
 __author__ = 'Administrator'
+#coding:utf-8
 
 from parseIni import *
 from parseIniFile import *
 
 def test0():
-    f = open("0.ini","rb")
-    print getValue(f,"global","port");
+    # 一般写法
+    # f = open("0.ini","rb")
+    # print getValue(f,"global","port");
+    # f.close()
+
+    # 自动管理close,try catch [with open as 从python2.5引入(需要通过 from __future__ import with_statement 导入后才可以使用),从 2.6 版本开始缺省可用]
+    with open("0.ini","rb") as f:
+        print getValue(f,"global","port");
 
 def test1():
-    f = open("1.ini","rb")
-    strFileContent = f.read()
-    f.close()
+    # 一般写法
+    # f = open("1.ini","rb")
+    # strFileContent = f.read()
+    # f.close()
+
+    # 自动管理close,try catch [with open as 从python2.5引入(需要通过 from __future__ import with_statement 导入后才可以使用),从 2.6 版本开始缺省可用]
+    with open("1.ini","rb") as f:
+        strFileContent = f.read()
+
     vardict = {}
-
     var1 = getPlatformMap(strFileContent)
-
     for k,v in var1.items():
         var2 = getSectionMap(v)
         dict3 = {}
